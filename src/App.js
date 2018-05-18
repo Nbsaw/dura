6715c3f60146902a637734c48627bfea4232d75d
-A- <nbsawz@gmail.com>
2018-05-19 01:57:31 +0800

    just enough good night guy !

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
 import React, { Component } from 'react'
 
 import './App.css'
+import Row from 'antd/lib/row'
+import Col from 'antd/lib/col'
 import Card from 'antd/lib/card'
 
-import { githubAPi } from './api'
+import { githubApi } from './api'
+import _ from 'lodash'
 
 class App extends Component {
-  render() {
-    let data = githubAPi.getAll({
+  state = {
+    list: [],
+    labels: []
+  }
+
+  async componentDidMount () {
+    let list = await githubApi.issues.getAll({
       username: 'Nbsaw',
       repo: 'notes'
     })
-    
+    let labels = {}
+    list.forEach(item => {
+      item.labels.forEach(label => {
+        !Array.isArray(labels[label.name]) && (labels[label.name] = [])
+        labels[label.name].push(item)
+      })
+    })
+    this.setState({ list, labels })
+  }
+
+  render() {
     return (
-      <div>
-        <Card title="Card title" extra={<a href="#">More</a>} style={{ width: 300 }}>
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
-        </Card>
-        <Card title="Card title" extra={<a href="#">More</a>} style={{ width: 300 }}>
-          <p>Card content</p>
-          <p>Card content</p>
-          <p>Card content</p>
-        </Card>
-      </div>
+      <Row gutter={16}>
+        {
+          _.map(this.state.labels, (list, name) => (
+            <Col span={4} style={{ marginBottom: '16px' }}>
+              <Card key={name} title={name}>
+                {
+                  list.map(post => (
+                    <p key={post.id}>{post.title}</p>
+                  ))
+                }
+              </Card>
+            </Col>
+          ))
+        }
+      </Row>
     );
   }
 }
