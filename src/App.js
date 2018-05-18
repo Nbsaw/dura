d3e8006f0cd665fa1c3a9f008d94c230fe2d5ac8
-A- <nbsawz@gmail.com>
2018-05-19 01:31:13 +0800

    add lodash

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
+import './App.css'
+import Card from 'antd/lib/card'
+
+import { githubAPi } from './api'
 
 class App extends Component {
   render() {
+    let data = githubAPi.getAll({
+      username: 'Nbsaw',
+      repo: 'notes'
+    })
+    
     return (
       <div>
-        hello world
+        <Card title="Card title" extra={<a href="#">More</a>} style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+        <Card title="Card title" extra={<a href="#">More</a>} style={{ width: 300 }}>
+          <p>Card content</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
       </div>
     );
   }
