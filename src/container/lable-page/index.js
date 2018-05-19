6e01d74da8c299e1bac43140946619bab1b6b66e
-A- <nbsawz@gmail.com>
2018-05-19 22:33:53 +0800

    add markdown it

--- src/container/lable-page/index.js
+++ src/container/lable-page/index.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import MarkdownIt from 'markdown-it'
+import { githubApi, github } from '../../api'
+
+const md = MarkdownIt()
 
 class LabelPage extends Component {
-  render () {
+  state = {
+    content: ''
+  }
+  async componentDidMount () {
     const { match } = this.props
+    const result = await githubApi.issues.getDetails({
+      username: 'Nbsaw',
+      repo: 'notes',
+      number: match.params.number
+    })
+    const content = md.render(result.body)
+    this.setState({ content })
+  }
+  render () {
     return (
-      <p>{match.params.number}</p>
+      <p dangerouslySetInnerHTML={{ __html: this.state.content }} />
     )
   }
 }
