fde03c86f5949f984812ae7533d1eed152f0000a
Gavin Phang <nbsawz@gmail.com>
2018-05-30 23:07:13 +0800

    Feature/home page (#5)
    * add all labels page
    
    * tweak
    
    * change style
    
    * add link
    
    * add markdown it
    
    * add theme
    
    * add styled-components dep
    
    * hiahiahia
    
    * tweak
    
    * add Blog link

---
+++ src/components/layout/aside/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Aside = () => (
+  <div>
+    Hello ~
+  </div>
+)
+
+export default Aside
--- src/container/home-page/index.js
+++ src/container/home-page/index.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react'
+import { Avatar, Container, Section, Description, Division, NikeName, Footer } from './styled'
+import SocicalList from '../../components/socical-list'
+
+const socialList = [
+  {
+    key: 'Blog',
+    link: '#/labels'
+  },
+  {
+    key: 'Zhihu',
+    link: 'https://www.zhihu.com/people/nbsaw',
+  },
+  {
+    key: 'Github',
+    link: 'https://github.com/Nbsaw',
+  },
+  {
+    key: 'Twitter',
+    link: 'https://twitter.com/nbsaw',
+  },
+  {
+    key: 'CodePen',
+    link: 'https://codepen.io/nbsaw'
+  }
+]
+
+const NikeNameSection = () => (
+  <Section>
+    <NikeName>Gavin Phang</NikeName>
+  </Section>
+)
+
+const AvatarSection = () => (
+  <Section>
+    <Avatar src="https://avatars1.githubusercontent.com/u/12481935?s=460&v=4" />
+  </Section>
+)
+
+const DescriptionSection = () => (
+  <Section>
+    <Description>
+      Gavin >> [ 'FE' , 'CS' , 'OTAKU' ]
+    </Description>
+  </Section>
+)
+
+const SocicalSection = () => (
+  <Section>
+    <SocicalList list={socialList} />
+  </Section>
+)
 
 class HomePage extends Component {
   render () {
     return (
-      <div>
-        <p>It's Home page guy ~</p>
-      </div>
+      <Container>
+        <AvatarSection />
+        <NikeNameSection />
+        <DescriptionSection />
+        <Division />
+        <SocicalSection />
+        <Footer>© 2018 nbsaw </Footer>
+      </Container>
     )
   }
 }
