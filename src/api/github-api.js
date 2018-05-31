326e19e035cef615adf0b82c43778ec7e64d0049
Gavin Phang <nbsawz@gmail.com>
2018-05-31 23:05:18 +0800

    Feature/add api flow (#7)
    * tewak
    
    * add missing flow type in component

--- src/api/github-api.js
+++ src/api/github-api.js
@@ -1,15 +1,20 @@
+// @flow
 import { GITHUB_API_URL } from '../constant'
 import { fetchGithubWithOauth } from './comman'
 import qs from 'qs'
 
+type GetRepoInfoParams = { username: string, repo: string }
+type GetAllParams = { username: string, repo: string, state: string }
+type GetDetailsParams = { username: string, repo: string, number: number }
+
 const github = {
-  getRepoInfo: async ({ username, repo }) => {
+  getRepoInfo: async ({ username , repo } : GetRepoInfoParams ) => {
     const request = await fetchGithubWithOauth.get(`${GITHUB_API_URL}/repos/${username}/${repo}`)
     const result = request.data
     return result
   },
   issues: {
-    getAll: async ({ username, repo, state = 'open' }) => {
+    getAll: async ({ username, repo, state = 'open' } : GetAllParams) => {
       const query = qs.stringify({ state })
       const request = await fetchGithubWithOauth.get(
         `${GITHUB_API_URL}/repos/${username}/${repo}/issues?${query}`
@@ -17,7 +22,7 @@ const github = {
       const result = request.data
       return result
     },
-    getDetails: async ({ username, repo, number }) => {
+    getDetails: async ({ username, repo, number } : GetDetailsParams) => {
       const request = await fetchGithubWithOauth.get(
         `${GITHUB_API_URL}/repos/${username}/${repo}/issues/${number}`
       )
