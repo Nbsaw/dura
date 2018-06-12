4501800b5402ce82e1740916fe62812c78160f8d
-A- <nbsawz@gmail.com>
2018-06-12 22:27:23 +0800

    tweak

--- src/blocks/PosterLoader/index.js
+++ src/blocks/PosterLoader/index.js
@@ -26,8 +26,7 @@ const Post = () => (
           primaryColor="#f3f3f3"
           secondaryColor="#ecebeb"
         >
-          <rect x="0" y="0" width="265" height={h} />
-          <rect x="275" y="0" width="400" height={h} />
+          <rect x="0" y="0" width="160" height={h} />
         </ContentLoader>
       </h2>
       <ContentLoader
@@ -53,27 +52,11 @@ const Post = () => (
 
         <rect x="0" y={17 * h} width="400" height={h} />
 
-        <rect x="0" y={19 * h} width="400" height={h} />
+        <rect x="0" y={19 * h} width="120" height={h} />
 
-        <rect x="0" y={21 * h} width="400" height={h} />
+        <rect x="0" y={21 * h} width="60" height={h} />
 
-        <rect x="0" y={23 * h} width="60" height={h * 5} />
-        <rect x="65" y={23 * h} width="400" height={h * 5} />
-
-        <rect x="0" y={29 * h} width="400" height={h} />
-        <rect x="0" y={31 * h} width="400" height={h} />
-
-
-        <rect x="0" y={33 * h} width="20" height={h} />
-        <rect x="30" y={33 * h} width="300" height={h} />
-        <rect x="335" y={33 * h} width="60" height={h} />
-
-        <rect x="0" y={35 * h} width="400" height={h} />
-
-        <rect x="0" y={37 * h} width="400" height={h * 8} />
-
-        <rect x="0" y={46 * h} width="400" height={h} />
-        <rect x="0" y={48 * h} width="400" height={h} />
+        <rect x="0" y={23 * h} width="30" height={h} />
       </ContentLoader>
     </Container>
   </Wrapper>
