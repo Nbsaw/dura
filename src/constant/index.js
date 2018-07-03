f304a984d224903f9cab6ac9112c03600a971d02
-A- <nbsawz@gmail.com>
2018-07-03 10:26:01 +0800

    update token

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,2 +1,3 @@
+export * from './me'
 export const GITHUB_API_URL = "https://api.github.com"
-export const GITHUB_OAUTH_TOKEN = "9b628ac5d398fb9f37463c3b94914c7a029c0301"
+export const GITHUB_OAUTH_TOKEN = "8ea5336acd7414268b0e2228892af2a5dccc3b66"
