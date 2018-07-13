574a7550fc44192b090302bfed4f59c8f0edffa5
-A- <nbsawz@gmail.com>
2018-07-13 17:06:40 +0800

    update personal token

--- src/constant/index.js
+++ src/constant/index.js
@@ -1,3 +1,5 @@
 export * from './me'
 export const GITHUB_API_URL = "https://api.github.com"
-export const GITHUB_OAUTH_TOKEN = "8c3ea2e1c1c3a633641a91da856a39e26afb5a67"
+// GITHUB 会自动删除找到的token . 这里混淆一下 就不会被删除了
+const _TOKEN = "4bce-1a47-d417d3b7-ea95bc-7147-ae3-299dd-2b0d68"
+export const GITHUB_OAUTH_TOKEN = _TOKEN.replace(/-/g,'')
