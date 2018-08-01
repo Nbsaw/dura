0461b97584962bcdbc10ebbe2ba8cef526a9c06f
-A- <nbsawz@gmail.com>
2018-08-01 21:52:44 +0800

    done

---
+++ src/blocks/LabelLoader.js
@@ -0,0 +1,54 @@
+// @flow
+import React from 'react'
+import ContentLoader from "react-content-loader";
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 1620px;
+`
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`
+
+const h = 4
+
+const LabelLoader = () => (
+  <Wrapper>
+    <Container>
+      <ContentLoader
+        height={400}
+        width={400}
+        speed={2}
+        primaryColor="#f3f3f3"
+        secondaryColor="#ecebeb"
+      >
+        <rect x="0" y="0" width="15" height={h} />
+        <rect x="0" y={2 * h} width="60" height={h} />
+        <rect x="0" y={4 * h} width="80" height={h} />
+        <rect x="0" y={6 * h} width="60" height={h} />
+        <rect x="0" y={8 * h} width="40" height={h} />
+        <rect x="0" y={10 * h} width="75" height={h} />
+
+        <rect x="0" y={14 * h} width="15" height={h} />
+        <rect x="0" y={16 * h} width="60" height={h} />
+        <rect x="0" y={18 * h} width="80" height={h} />
+        <rect x="0" y={20 * h} width="60" height={h} />
+        <rect x="0" y={22 * h} width="40" height={h} />
+        <rect x="0" y={24 * h} width="75" height={h} />
+        
+        <rect x="0" y={28 * h} width="15" height={h} />
+        <rect x="0" y={30 * h} width="60" height={h} />
+        <rect x="0" y={32 * h} width="80" height={h} />
+        <rect x="0" y={34 * h} width="60" height={h} />
+        <rect x="0" y={36 * h} width="40" height={h} />
+        <rect x="0" y={38 * h} width="75" height={h} />
+        <rect x="0" y={40 * h} width="95" height={h} />
+      </ContentLoader>
+    </Container>
+  </Wrapper>
+);
+
+export default LabelLoader;
--- src/blocks/Lables.js
+++ src/blocks/Lables.js
@@ -1,14 +1,8 @@
 // @flow
-// TODO: 分离逻辑
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import _ from 'lodash'
 
-import { USERNAME, REPO } from 'constant'
-import { githubApi } from 'api'
-
-type Props = {}
-type State = { labels: {} }
 type LableTitleParams = { name: string, color: string }
 type PostTitleParams = { number: number, title: string}
 
@@ -26,44 +20,13 @@ const PostTitle = ({ number, title } : PostTitleParams) => (
   </p>
 )
 
-class Lables extends Component<Props,State> {
-  state = {
-    labels: {}
-  }
-  
-  async componentDidMount () {
-    let resultList = await githubApi.issues.getAll({
-      username: USERNAME,
-      repo: REPO
-    })
-    let labels = {}
-    resultList.forEach(item => {
-      item.labels.forEach(({ name, color }) => {
-        !(labels[name] !== null && typeof labels[name] === 'object') && (labels[name] = {})
-        !Array.isArray(labels[name]['items']) && (labels[name]['items'] = [])
-        labels[name]['items'].push(item)
-        labels[name]['color'] = `#${color}`
-      })
-    })
-    this.setState({ labels })
-  }
-  
-  render() {
-    return (
-      <div style={{ padding: '16px', height: '100vh', overflow: 'auto', flex: 1 }}>
-        {
-          _.map(this.state.labels, ({ items, color }, name) => (
-            <div key={name} style={{ marginBottom: '16px' }}>
-              <LabelTitle name={name} color={color} />
-              {
-                items.map((post, idx) => (<PostTitle key={idx} {...post} />))
-              }
-            </div>
-          ))
-        }
-      </div>
-    );
-  }
-}
+const Lables = ({ labels }) => _.map(labels, ({ items, color }, name) => (
+  <div key={name} style={{ marginBottom: '16px' }}>
+    <LabelTitle name={name} color={color} />
+    {
+      items.map((post, idx) => (<PostTitle key={idx} {...post} />))
+    }
+  </div>
+))
 
 export default Lables
