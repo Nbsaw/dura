a1ed462a6da5cb64bf8449df03a956aef15b6a51
wen <nbsawz@gmail.com>
2018-09-29 17:21:55 +0800

    fix aside in mobile dislocation

--- src/blocks/SocicalList.js
+++ src/blocks/SocicalList.js
@@ -5,6 +5,12 @@ import A from 'elements/A';
 const Navigation = styled.ul`
   padding: 0;
   margin: 0;
+  display: flex;
+  flex-direction: row-reverse;
+  min-width: 280px;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
   li {
     display: inline-block;
     list-style: none;
