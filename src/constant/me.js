26671fb2bcf76018498e1514f7334a45e488e152
wen <nbsawz@gmail.com>
2018-09-29 15:24:19 +0800

    Add Timeline in aside

--- src/constant/me.js
+++ src/constant/me.js
@@ -11,4 +11,5 @@ export const SOCICAL_LIST = [
   { key: 'Twitter', link: 'https://twitter.com/nbsaw' },
   { key: 'CodePen', link: 'https://codepen.io/nbsaw' },
   { key: 'Works', link: '#/works' },
+  { key: 'TimeLine', link: '#/timeline' },
 ];
