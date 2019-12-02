64fcd8c51b8ce592f5e6fdbcb39d85c5fe9e9c56
yuxino <nbsawz@gmail.com>
2019-12-02 15:03:38 +0800

    [feat] shit

--- src/pages/PostsPage/index.tsx
+++ src/pages/PostsPage/index.tsx
@@ -3,20 +3,56 @@ import styled from 'styled-components';
 import { githubApi } from '../../api';
 import SiteTitle from '../../elements/SiteTitle';
 import removeMd from 'remove-markdown';
+import PostTitle from '../TagsPage/components/PostTitle';
+import Icon from 'antd/lib/icon';
+import { Link } from 'react-router-dom';
 
 const Container = styled.div`
   padding-top: 20px;
   padding-bottom: 20px;
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 const Block = styled.div`
-  font-size: 15px;
-  & + & {
-    margin-top: 10px;
+  font-size: 12px;
+  display: flex;
+  flex-shrink: 0;
+  /* height: 400px; */
+  padding: 50px 25px;
+  border: 1px solid #ece8e8;
+  margin-top: 20px;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  &:nth-child(1n) {
+    margin-right: 20px;
   }
 `;
 
-const Text = styled.a``;
+const BlockText = styled.div`
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const BlockTextTitle = styled.h2``;
+
+const BlockImage = styled.img`
+  width: 300px;
+  height: 200px;
+  flex-shrink: 0;
+  margin-left: 25px;
+`;
+
+const BlockReadMore = styled(Link)`
+  width: 100%;
+`;
+
+const PostAt = styled.div`
+  margin-bottom: 15px;
+`;
+
+// https://picsum.photos/seed/picsum/200/300
 
 class PostsPage extends Component<{}, { resultList: any[] }> {
   state = {
@@ -50,14 +86,25 @@ class PostsPage extends Component<{}, { resultList: any[] }> {
           }: {
             updated_at: string;
             title: string;
-            number: number;
+            number: string;
             body: string;
           }) => {
             const plainText = removeMd(body);
             return (
               <Block key={number}>
-                {updated_at.slice(0, 10)} - <Text>{title}</Text>
-                <div>{plainText.slice(0, 150)}</div>
+                <BlockText>
+                  <PostAt>发布于 {updated_at.slice(0, 10)}</PostAt>
+                  <PostTitle number={number} title={title} />
+                  <p>{`${plainText.slice(0, 120)} ...`}</p>
+                </BlockText>
+                <div>
+                  <BlockImage
+                    src={`https://picsum.photos/500/300?number=${number}`}
+                  />
+                </div>
+                <BlockReadMore to={`post/${number}`}>
+                  Read More ...
+                </BlockReadMore>
               </Block>
             );
           }
