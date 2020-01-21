8a6e211b01a74347f6e2d86ab1e5ce9e46ebab73
yuxino <nbsawz@gmail.com>
2020-01-21 10:44:06 +0800

    feat: new time line events

--- src/constant/timeline.tsx
+++ src/constant/timeline.tsx
@@ -10,7 +10,43 @@ interface TimelineType {
 }
 
 const Timeline: TimelineType = {
+  2020: [
+    {
+      text: '01月21日 嗨，今天还在上班啊啊啊啊 还有两天 马上新的一年了哦 !'
+    },
+    {
+      text: '01月20日 丢四天没背单词了'
+    },
+    {
+      text: '01月19日 钱没有 又多了一张浦发银行的银行卡'
+    },
+    {
+      text: '01月17日 Leetcode 刷了57题了 目标是100题 冲呀  ！！'
+    },
+    {
+      text:
+        '01月01日 又开始天天背单词了 丢雷楼某 今年还是许愿脱单 有几个里程碑已经快达到了 奥利给'
+    }
+  ],
   2019: [
+    {
+      text: '11月02日 看了天气之子呢'
+    },
+    {
+      text: '09月xx日 去了趟广州玩 呜呜呜手机进水了 修了2k 妈蛋 又是一年的花呗'
+    },
+    {
+      text: '09月18日 贵州到咯 ~'
+    },
+    {
+      text: '09月13日 GO 重庆 !'
+    },
+    {
+      text: '09月12日 大熊猫啊啊啊啊啊'
+    },
+    {
+      text: '09月11日 上飞机咯 前往成都 !'
+    },
     {
       text: '06月29日 又开始锻炼身体了这一次可以持续多久呢..'
     },
@@ -24,7 +60,7 @@ const Timeline: TimelineType = {
       text: '04月15日 好像是今天来着拿到了驾照'
     },
     {
-      text: '05月08日 生日快乐 礼物是 airpods 和 电动牙刷 '
+      text: '05月08日 生日快乐'
     },
     {
       text: '03月19日 重新开始吧'
@@ -75,23 +111,8 @@ const Timeline: TimelineType = {
       text: '07月27日 科目二考试失败 留下了没有技术的泪水'
     },
     {
-      text: '中间 思考人生的时期,迄今为止过的最不快乐的时间'
-    },
-    {
-      dot: <Icon type="clock-circle-o" style={{ color: 'red' }} />,
-      text: '07月01日 离开深圳集智网络服务有限公司'
-    },
-    {
-      dot: <Icon type="smile-o" />,
-      text: '06月30日 毕业惹 成功拿到毕业证'
-    },
-    {
       color: 'green',
       text: '06月04日 Dura项目启动'
-    },
-    {
-      dot: <Icon type="fire" style={{ color: 'red' }} />,
-      text: '05月08日 眼睁睁看着自己20岁了'
     },
     {
       text: '03月24日 前往广州参加大疆校园招聘 (晚点了,变成了去广州玩)'
@@ -105,10 +126,6 @@ const Timeline: TimelineType = {
     {
       text:
         '02月28日 使用Trello作为看板 开始规划自己的目标 开始不停的想办法提高效率'
-    },
-    {
-      dot: <Icon type="clock-circle-o" />,
-      text: '02月22日 加入深圳集智网络服务有限公司'
     },
     {
       text: '02月04日 和基友搬入宝安灵芝 开启自己在外面住的生活'
@@ -133,15 +150,14 @@ const Timeline: TimelineType = {
       text: '06月11日 开始使用Github Issues写博客'
     },
     {
-      dot: <Icon type="fire" style={{ color: 'red' }} />,
-      text: '05月08日 眼睁睁看着自己19岁了'
-    },
-    {
       text:
         '01月26日 加入人生的第一个Orgnazation: Balde / 发起了人生的第一个Pull Request 到Blade项目'
     }
   ],
   2016: [
+    {
+      text: '12月xx日 看了你的名字'
+    },
     {
       text:
         '10月22日 博客启动 (这是能找到的最早的一条有效日期，虽然之前也在写博客)'
@@ -149,10 +165,6 @@ const Timeline: TimelineType = {
     {
       text:
         '04月17日 购买了人生的第一条索尼耳机 充值一波信仰 打了人生第一次白条'
-    },
-    {
-      dot: <Icon type="fire" style={{ color: 'red' }} />,
-      text: '05月08日 眼睁睁看着自己18岁了'
     },
     {
       dot: <Icon type="meh-o" style={{ color: 'orange' }} />,
@@ -169,19 +181,11 @@ const Timeline: TimelineType = {
     },
     {
       text: '05月17日 加入Github'
-    },
-    {
-      dot: <Icon type="fire" style={{ color: 'red' }} />,
-      text: '05月08日 眼睁睁看着自己17岁了'
     }
   ],
   2014: [
     {
       text: 'xx月xx日 反正是一个暑假 加入工作室'
-    },
-    {
-      dot: <Icon type="fire" style={{ color: 'red' }} />,
-      text: '05月08日 眼睁睁看着自己16岁了'
     }
   ]
 };
