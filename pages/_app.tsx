2de12fed920efafb828c8049e97abb676a2d0639
yuxino <nbsawz@gmail.com>
2020-04-02 14:26:11 +0800

    feat: new style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,20 @@ interface CustomAppProps {
   };
 }
 
+console.log(
+  "%c ",
+  `
+    background:url(https://pics.manqian.com/mq-pim/202004021585808377599.png) no-repeat;
+    background-size: 100%;
+    padding-left:200px;
+    padding-bottom:100px;
+    border-radius:10px;
+    margin:5px 0;
+  `
+);
+console.log("EI HEI, WHY YOU OPEN THE DEVTOOLS ?");
+console.log("WOULD YOU LIKE TO SEE THE CODE FIRST OR EAT SOME KEMOMIMI ...");
+
 // getInitialProps , layout
 function App({ Component, pageProps }: AppProps & CustomAppProps) {
   console.log(Component.layout?.name);
